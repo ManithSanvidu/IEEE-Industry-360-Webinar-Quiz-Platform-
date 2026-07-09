c0113fecdf14408c63f07a89e48e809bf4ce5069
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-07-09 22:54:42 +0530

    Add files via upload

--- src/components/DragonBackground.js
+++ src/components/DragonBackground.js
@@ -12,7 +12,7 @@ export default function DragonBackground() {
       size: Math.random() * 6 + 2,
       delay: Math.random() * 5,
       duration: Math.random() * 3 + 2,
-      color: ['#C026D3', '#F97316', '#F5B942', '#1F4E79'][Math.floor(Math.random() * 4)]
+      color: ['#F5C05B', '#E86C3B', '#1C3652', '#EAE0C8'][Math.floor(Math.random() * 4)]
     }));
     // eslint-disable-next-line react-hooks/set-state-in-effect
     setParticles(p);
@@ -20,9 +20,9 @@ export default function DragonBackground() {
 
   return (
     <div className="dragon-bg">
-      <div className="dragon-bg-shape" style={{ width: 400, height: 400, top: '-10%', right: '-5%', background: '#C026D3' }} />
-      <div className="dragon-bg-shape" style={{ width: 300, height: 300, bottom: '-5%', left: '-5%', background: '#F97316' }} />
-      <div className="dragon-bg-shape" style={{ width: 250, height: 250, top: '40%', left: '30%', background: '#1F4E79' }} />
+      <div className="dragon-bg-shape" style={{ width: 400, height: 400, top: '-10%', right: '-5%', background: '#1C3652' }} />
+      <div className="dragon-bg-shape" style={{ width: 300, height: 300, bottom: '-5%', left: '-5%', background: '#0A1118' }} />
+      <div className="dragon-bg-shape" style={{ width: 350, height: 350, top: '40%', left: '30%', background: '#101E2E' }} />
       {particles.map(p => (
         <div key={p.id} className="dragon-particle" style={{
           left: `${p.left}%`, top: `${p.top}%`,
